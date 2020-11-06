440f30aec64da0b8550fbb565554dcf7d9f58dfe
Sean Shadle <seandshadle@gmail.com>
2020-11-06 12:31:23 -0600

    Actually completed mvp

--- models/tasks.js
+++ models/tasks.js
@@ -6,9 +6,19 @@ module.exports = {
 }
 
 function getTasks(){
-    return db("tasks as t").join('projects as p', "t.project_id", "p.id").select("p.name as project_name", "p.description as project_description", "t.*");
+    return db("tasks as t")
+    .join('projects as p', "t.project_id", "p.id")
+    .select("p.name as project_name", "p.description as project_description", "t.*");
 }
 
 function getTasksById(id){
     return db("tasks").where({id}).first();
+}
+
+function addTask(data){
+    return db("tasks")
+        .insert(data)
+        .then(id => {
+            return getTasksById(id);
+        })
 }
